17b7e64f3705cd180f6cebd0dbee7c2d6d1cde49
Shreyas Moudgalya <smoudgal@jarvis.cs.iit.edu>
2016-10-02 22:36:29 -0500

    global

--- hw2/pthreadGauss.c
+++ hw2/pthreadGauss.c
@@ -3,64 +3,25 @@
 #include<stdlib.h>
 
 #define NUM_THREADS 16
-int num;
+int n;
+//int p = NUM_THREADS;
 
+volatile float A[20][20], b[10], x[10];
 
-pthread_barrier_t phase_barrier, row_barrier;
+struct s1 {
+  int norm;
+  int id;
+};
 
-// following are function declarations
-
-void* thrdfunc(void* p);
-
-// function declaration
-
-void* thrdfunc(void* p)
-{
-
-  float* A = p;
-  int thread_number = pthread_self();
-  int k,j,i;
-
-  for(k=0; k<=num-1;k++)
-    {
-
-      pthread_barrier_wait(&row_barrier);
-
-      for(j = (k+1+thread_number); j<=num-1 ; j=(j+NUM_THREADS))
-	{
-	  *((A+k*num)+j) = *((A+k*num)+j) / *((A+k*num)+k);
-	 
-	  for(i=k+1; i<=num-1; i++)
-	    {
-	      *((A+i*num)+j) = *((A+i*num)+j) - (*((A+i*num)+k) * *((A+k*num)+j));
-	    }
-	}
-    }
-  
-  pthread_barrier_wait(&phase_barrier);
-}
-
-
-
+void* thrdfunc(void *p);
 
 
 int main()
 {
-  int i,j,k,n, tn;
-
-  float A[20][20], b[10], x[10], y[10];
-  
-  int p = NUM_THREADS; //number of threads
-  pthread_t threads[p];
-
-  pthread_barrier_init(&phase_barrier, NULL, p); 
-  pthread_barrier_init(&row_barrier,NULL,p);
-
-
+  int i,j,k;
+ 
   printf("\nEnter the order of matrix: ");
   scanf("%d",&n);
-  
-  num = n;
 
   printf("\nEnter the elements of augmented matrix row-wise:\n\n");
   for(i=0; i<n; i++)
@@ -80,41 +41,40 @@ int main()
       scanf("%f",&b[i]);
     }
 
-  for(tn=0; tn<p;tn++)
+
+
+  for (k = 0; k < n-1; k++) 
     {
-      pthread_create(&threads[tn],NULL,thrdfunc, (void* )A);
-    }
-  
-  for(tn=0; tn<p; tn++)
-    {
-      pthread_join(threads[tn],NULL);
-    }
-  
-  pthread_barrier_wait(&phase_barrier);
-  
-  for(k=0; k<=n-1;k++)
-    {
-      
-      y[k] = b[k]/A[k][k];
-      A[k][k] = 1;
-    
-      for(i=k+1; i<=n-1;i++)
+      pthread_t threads[n];
+
+      struct s1* para = malloc(2 * sizeof(struct s1));
+      for (i = 0; i < p; i++) 
 	{
-	  b[i] = b[i] - A[i][k] * y[k];
-	  A[i][k] = 0;
+	  para[i].norm = k;
+	  para[i].id = i;
+	  
+	  pthread_create(&threads[i], NULL, thrdfunc, (void*) &para[i]);
 	}
+
+      for (j = 0; j < p; j++) 
+	{
+	  pthread_join(threads[j], NULL);
+	}
+
+      free(para);
     }
-  
- 
-  // Backward Substitution
-  for(k=n-1; k>=0; k--)
-    {
-      x[k] = y[k];
-      for(i=k-1;i>=0;i--)
-        {
-	  y[i] = y[i] - A[i][k] * x[k];
-        }
+
+  int row, col;
+
+
+  /* Back substitution */
+  for (row = n - 1; row >= 0; row--) {
+    x[row] = b[row];
+    for (col = n-1; col > row; col--) {
+      x[row] -= A[row][col] * x[col];
     }
+    x[row] /= A[row][row];
+  }
 
 
   printf("\nThe solution is: \n");
@@ -127,4 +87,24 @@ int main()
   printf("\n");
 
   return(0);
+}
+
+
+
+void* thrdfunc(void *s)
+{ 
+  struct s1* myStruct = (struct s1*) s;
+  int norm = myStruct->norm;
+  int id = myStruct->id;
+  int i;
+  for(i=norm+id;i<n;i=i+NUM_THREADS) {
+    int col; 
+    float multiplier;
+    multiplier = A[i][norm] / A[norm][norm];
+    for (col = norm; col < n; col++) {
+      A[i][col] -= A[norm][col] * multiplier;
+    }
+    b[i] -= b[norm] * multiplier;
+  }
+  pthread_exit(0);
 }

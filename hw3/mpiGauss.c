694eaef117a43b365e72b6048eb2a4bde12e9fb4
Shreyas Moudgalya <smoudgal@jarvis.cs.iit.edu>
2016-10-22 19:06:49 -0500

    Working HW3

--- hw3/mpiGauss.c
+++ hw3/mpiGauss.c
@@ -8,12 +8,14 @@ int main(int argc, char **argv)
   MPI_Init(&argc, &argv);
 
   int i,j,k;
-  int map[500];
-  double A[500][500],b[500],c[500],x[500],sum=0.0;
+  int map[2000];
+  double A[2000][2000],b[2000],c[2000],x[2000],sum=0.0;
   double range=1.0;
-  int n=3;
+  int n=500;
   int rank, nprocs;
-  clock_t begin1, end1, begin2, end2;
+  //  clock_t begin1, end1, begin2, end2;
+  double begin1, end1;
+  double begin2, end2;
   MPI_Status status;
 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);   // get current process id 
@@ -42,7 +44,8 @@ int main(int argc, char **argv)
 
 
 
-  begin1 =clock();
+  //begin1 =clock();
+  begin1 = MPI_Wtime();
 
   MPI_Bcast (&A[0][0],500*500,MPI_DOUBLE,0,MPI_COMM_WORLD);
   MPI_Bcast (b,n,MPI_DOUBLE,0,MPI_COMM_WORLD);    
@@ -72,14 +75,16 @@ int main(int argc, char **argv)
                 {
 		  A[i][j]=A[i][j]-( c[i]*A[k][j] );
                 }
-	      b[i]=b[i]-( c[i]*b[k] );
+	      b[i]=b[i]-(c[i]*b[k]);
             }
         }
     }
-  end1 = clock();
+  //  end1 = clock();
+  end1 = MPI_Wtime();
 
+  //  begin2 =clock();
+  begin2 = MPI_Wtime();
 
-  begin2 =clock();
   if (rank==0)
     { 
       x[n-1]=b[n-1]/A[n-1][n-1];
@@ -94,7 +99,8 @@ int main(int argc, char **argv)
 	  x[i]=(b[i]-sum)/A[i][i];
         }
 
-      end2 = clock();
+      //      end2 = clock();
+      end2 = MPI_Wtime();
     }
 
   if (rank==0)
@@ -106,8 +112,9 @@ int main(int argc, char **argv)
 
         }
 
-      printf("\n\nLU decomposition time: %f", (double)(end1 - begin1) / CLOCKS_PER_SEC);
-      printf("\nBack substitution time: %f\n", (double)(end2 - begin2) / CLOCKS_PER_SEC);
+      printf("\n\nLU decomposition time: %f", (double)(end1 - begin1));
+      printf("\n");
+      //printf("\nBack substitution time: %f\n", (double)(end2 - begin2));
     }
   MPI_Finalize();
   return(0);
